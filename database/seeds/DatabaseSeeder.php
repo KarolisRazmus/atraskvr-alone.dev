0f3139cb95b31ae8f63cb93ef73be882866baae7
KarolisRazmus <karolisrazmus@gmail.com>
2017-06-08 17:09:58 +0300

    added seeds

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(LanguagesSeeder::class);
+        $this->call(PagesCategoriesSeeder::class);
+        $this->call(RolesSeeder::class);
     }
 }
