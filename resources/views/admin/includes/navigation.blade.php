1866d6ddcb65179dd1b896e5e1067998536ba3d8
KarolisRazmus <karolisrazmus@gmail.com>
2017-06-12 14:30:25 +0300

    added navigation to orders, reservations and users

--- resources/views/admin/includes/navigation.blade.php
+++ resources/views/admin/includes/navigation.blade.php
@@ -35,7 +35,19 @@
         @endif
         ">
         <a href="{{route('app.orders.index')}}">
-            <i class="fa fa-language"></i> <span>Orders</span>
+            <i class="fa fa-list-ul"></i> <span>Orders</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+        </a>
+    </li>
+    <li class="treeview
+        @if($tableName == 'reservations')
+            active
+        @endif
+            ">
+        <a href="{{route('app.reservations.index')}}">
+            <i class="fa fa-calendar"></i> <span>Reservations</span>
             <span class="pull-right-container">
               <i class="fa fa-angle-left pull-right"></i>
             </span>
@@ -48,6 +60,18 @@
         ">
         <a href="{{route('app.languages.index')}}">
             <i class="fa fa-language"></i> <span>Languages</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+        </a>
+    </li>
+    <li class="treeview
+        @if($tableName == 'users')
+            active
+        @endif
+            ">
+        <a href="{{route('app.users.index')}}">
+            <i class="fa fa-users"></i> <span>Users</span>
             <span class="pull-right-container">
               <i class="fa fa-angle-left pull-right"></i>
             </span>
