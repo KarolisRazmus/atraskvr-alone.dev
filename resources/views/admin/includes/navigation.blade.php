de9b27e9d7e3c28d90bdda7fcfb75cf9d78ae240
KarolisRazmus <karolisrazmus@gmail.com>
2017-06-12 11:46:18 +0300

    added "active" class to navigation

--- resources/views/admin/includes/navigation.blade.php
+++ resources/views/admin/includes/navigation.blade.php
@@ -1,24 +1,28 @@
 <!-- sidebar menu: : style can be found in sidebar.less -->
 <ul class="sidebar-menu">
     <li class="header">MAIN NAVIGATION</li>
-    <li class="treeview">
-        <a href="#">
+    <li class="treeview
+        @if($tableName == 'pages')
+            active
+        @endif
+        ">
+        <a href="{{route('app.pages.index')}}">
             <i class="fa fa-files-o"></i> <span>Pages</span>
             <span class="pull-right-container">
               <i class="fa fa-angle-left pull-right"></i>
             </span>
         </a>
-        <ul class="treeview-menu">
-            <li><a href="http://atraskvr-alone.dev/admin/pages"><i class="fa fa-circle-o"></i> List</a></li>
-            <li><a href="index2.html"><i class="fa fa-circle-o"></i> View</a></li>
-        </ul>
+        {{--<ul class="treeview-menu">--}}
+            {{--<li><a href="http://atraskvr-alone.dev/admin/pages"><i class="fa fa-circle-o"></i> List</a></li>--}}
+            {{--<li><a href="index2.html"><i class="fa fa-circle-o"></i> View</a></li>--}}
+        {{--</ul>--}}
     </li>
     <li class="treeview
         @if($tableName == 'pages_categories')
             active
         @endif
         ">
-        <a href="#">
+        <a href="{{route('app.pages_categories.index')}}">
             <i class="fa fa-folder-open-o"></i> <span>Pages categories</span>
             <span class="pull-right-container">
               <i class="fa fa-angle-left pull-right"></i>
@@ -30,23 +34,19 @@
             active
         @endif
         ">
-        <a href="#">
+        <a href="{{route('app.orders.index')}}">
             <i class="fa fa-language"></i> <span>Orders</span>
             <span class="pull-right-container">
               <i class="fa fa-angle-left pull-right"></i>
             </span>
         </a>
     </li>
-    <li class="treeview">
-        <a href="#">
-            <i class="fa fa-language"></i> <span>Languages</span>
-            <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
-        </a>
-    </li>
-    <li class="treeview">
-        <a href="#">
+    <li class="treeview
+        @if($tableName == 'languages')
+            active
+        @endif
+        ">
+        <a href="{{route('app.languages.index')}}">
             <i class="fa fa-language"></i> <span>Languages</span>
             <span class="pull-right-container">
               <i class="fa fa-angle-left pull-right"></i>
