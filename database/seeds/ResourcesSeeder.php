d527366eb600d4ecf093cd6cdaaad6b0ec4989cc
KarolisRazmus <karolisrazmus@gmail.com>
2017-06-15 09:59:07 +0300

    fixed file path

--- database/seeds/ResourcesSeeder.php
+++ database/seeds/ResourcesSeeder.php
@@ -21,8 +21,8 @@ class ResourcesSeeder extends Seeder
     {
         $list = [
             ["id" => Uuid::uuid4(), "mime_type" => "image/jpeg", "path" => "upload/2017/06/12/1497255944_the_lab.jpg", "size" => "67079"], // Manage everything
-            ["id" => Uuid::uuid4(), "mime_type" => "image/jpeg", "path" => "upload/2017/06/12/1497268016_tilt_brush.jpg", "size" => "67058"], // Manage everything
-            ["id" => Uuid::uuid4(), "mime_type" => "image/jpeg", "path" => "upload/2017/06/12/1497268016_hurl.jpg", "size" => "85379"], // Manage everything
+            ["id" => Uuid::uuid4(), "mime_type" => "image/jpeg", "path" => "upload/2017/06/12/1497266407_tilt_brush.jpg", "size" => "67058"], // Manage everything
+            ["id" => Uuid::uuid4(), "mime_type" => "image/jpeg", "path" => "upload/2017/06/12/1497266455_hurl.jpg", "size" => "85379"], // Manage everything
             ["id" => Uuid::uuid4(), "mime_type" => "image/jpeg", "path" => "upload/2017/06/12/1497266467_final_goalie_football_simulator.jpg", "size" => "70614"], // Manage everything
             ["id" => Uuid::uuid4(), "mime_type" => "image/jpeg", "path" => "upload/2017/06/12/1497266446_ktu_parasparnis.jpg", "size" => "47075"], // Manage everything
             ["id" => Uuid::uuid4(), "mime_type" => "image/jpeg", "path" => "upload/2017/06/12/1497266434_samsung_irklavimas.jpg", "size" => "71341"], // Manage everything
