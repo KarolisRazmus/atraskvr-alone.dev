3febfcaa6ac848903bf35c9c298f817f32ae9ab0
KarolisRazmus <karolisrazmus@gmail.com>
2017-06-16 00:37:38 +0300

    done remejai commit

--- resources/views/front-end/pages/sponsors.blade.php
+++ resources/views/front-end/pages/sponsors.blade.php
@@ -1,27 +1,27 @@
 <div>
-    <div class="col-xs-12 col-sm-offset-4 col-sm-4 col-md-offset-4 col-md-4 col-lg-offset-4 col-lg-4"><h2 class="page_title_h2">Remejai</h2></div>
+    <div class="col-xs-12"><h2 class="page_title_h2">Remėjai</h2></div>
 
-    <div class="col-xs-12 col-sm-offset-4 col-sm-4 col-md-offset-4 col-md-4 col-lg-offset-4 col-lg-4"><h4 class="page_title_h2">Ikvepia:</h4></div>
+    <div class="col-xs-12"><h4 class="page_sub_title_h4">Įkvepia:</h4></div>
 
     <div class="row">
-        <div class="col-xs-offset-1 col-xs-10 col-sm-offset-3 col-sm-6 col-md-offset-4 col-md-4 col-lg-offset-4 col-lg-4"><img class="sponsors_logos img-responsive center-block" src="{{asset('images/elektromarkt_logo.png')}}" alt=""></div>
+        <div class="col-xs-offset-1 col-xs-10 col-sm-offset-3 col-sm-6 col-md-offset-3 col-md-6 col-lg-offset-3 col-lg-6"><img class="sponsors_logos img-responsive center-block" src="{{asset('images/elektromarkt_logo.png')}}" alt=""></div>
     </div>
 
-    <div class="col-xs-12 col-sm-offset-4 col-sm-4 col-md-offset-4 col-md-4 col-lg-offset-4 col-lg-4"><h4 class="page_title_h2">Informuoja:</h4></div>
+    <div class="col-xs-12"><h4 class="page_sub_title_h4">Informuoja:</h4></div>
 
     <div class="row">
         <div class="col-xs-offset-1 col-xs-10 col-sm-offset-3 col-sm-6 col-md-offset-4 col-md-4 col-lg-offset-4 col-lg-4"><img class="sponsors_logos img-responsive center-block" src="{{asset('images/delfi_logo.png')}}" alt=""></div>
     </div>
 
-    <div class="col-xs-12 col-sm-offset-4 col-sm-4 col-md-offset-4 col-md-4 col-lg-offset-4 col-lg-4"><h4 class="page_title_h2">Organizuoja:</h4></div>
+    <div class="col-xs-12"><h4 class="page_sub_title_h4">Organizuoja:</h4></div>
 
     <div class="row">
-        <div class="col-xs-offset-1 col-xs-10 col-sm-offset-3 col-sm-6 col-md-offset-4 col-md-4 col-lg-offset-9 col-lg-4"><img class="sponsors_logos img-responsive center-block" src="{{asset('images/akropolis_logo.jpg')}}" alt=""></div>
-        <div class="col-xs-offset-1 col-xs-10 col-sm-4 col-md-3 col-lg-4"><img class="sponsors_logos img-responsive center-block" src="{{asset('images/litexpo_logo.png')}}" alt=""></div>
-        <div class="col-xs-offset-1 col-xs-10 col-sm-4 col-md-3 col-lg-4"><img class="sponsors_logos img-responsive center-block" src="{{asset('images/telesoftas_logo.png')}}" alt=""></div>
+        <div class="col-xs-offset-1 col-xs-10 col-sm-offset-2 col-sm-3 col-md-offset-2 col-md-3 col-lg-offset-2 col-lg-3"><img class="sponsors_logos img-responsive center-block" src="{{asset('images/akropolis_logo.jpg')}}" alt=""></div>
+        <div class="col-xs-offset-1 col-xs-10 col-sm-offset-0 col-sm-2 col-md-offset-0 col-md-2 col-lg-offset-0 col-lg-2"><img class="sponsors_logos img-responsive center-block" src="{{asset('images/litexpo_logo.png')}}" alt=""></div>
+        <div class="col-xs-offset-1 col-xs-10 col-sm-offset-0 col-sm-3 col-md-offset-0 col-md-3 col-lg-offset-0 col-lg-3"><img class="sponsors_logos img-responsive center-block" src="{{asset('images/telesoftas_logo.png')}}" alt=""></div>
     </div>
 
-    <div class="col-xs-12 col-sm-offset-4 col-sm-4 col-md-offset-4 col-md-4 col-lg-offset-4 col-lg-4"><h4 class="page_title_h2">Inovacijas palaiko:</h4></div>
+    <div class="col-xs-12"><h4 class="page_sub_title_h4">Inovacijas palaiko:</h4></div>
 
     <div class="row">
         <div class="col-xs-offset-1 col-xs-10 col-sm-offset-3 col-sm-6 col-md-offset-4 col-md-4 col-lg-offset-4 col-lg-4"><img class="sponsors_logos img-responsive center-block" src="{{asset('images/inida_logo.png')}}" alt=""></div>
