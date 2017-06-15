d48ef8f56b666095fc90bdadfc8daefb66d12867
KarolisRazmus <karolisrazmus@gmail.com>
2017-06-15 15:46:54 +0300

    added lang aap files

---
+++ resources/lang/en/app.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+   'inspired_by' => 'Inspired by: ',
+   'main_title' => 'Discover VR',
+   'header_description' => 'Pasmatry VR vdrugymi glazamy (English)',
+   'language' => 'Language'
+
+
+];
